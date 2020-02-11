d69caa66e69d38210b293a77e91d7faaafaca01d
Alfred Dominic <mailme@alfiemax.com>
2020-02-11 13:23:21 +0530

    updates to support osx

--- scorer.py
+++ scorer.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python2
+
+import os
 import requests
 from bs4 import BeautifulSoup
-import pynotify
 from time import sleep
+
 def sendmessage(title, message):
-    pynotify.init("Test")
-    notice = pynotify.Notification(title, message)
-    notice.show()
-    return
+  os.system("osascript -e 'display notification \"" + message + "\"\'" + "'with title \"" + title + "\"\'")
+  return
+
 url = "http://static.cricinfo.com/rss/livescores.xml"
+
 while True:
+  r = requests.get(url)
+  while r.status_code is not 200:
     r = requests.get(url)
-    while r.status_code is not 200:
-            r = requests.get(url)
-    soup = BeautifulSoup(r.text)
-    data = soup.find_all("description")
-    score = data[1].text
-    sendmessage("Score", score)
-    sleep(10)
+  soup = BeautifulSoup(r.text)
+  data = soup.find_all("description")
+  score = data[2].text
+  sendmessage("Score", score)
+  sleep(10)
